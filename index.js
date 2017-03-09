403b8dd2b42fb79a8e709bd3356c2dd5d9ad6a59
Benjamin Cantlupe <bjcantlupe@gmail.com>
2017-03-09 01:53:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
 function getIssues() {
+  fetch("https://api.github.com/repos/BeejLuig/javascript-fetch-lab/issues").then(res => res.json()).then(json => showIssues(json));
 }
 
 function showIssues(json) {
+  var src = document.getElementById("issues-template").innerHTML;
+  var template = Handlebars.compile(src);
+  var html = template(json);
+  console.log(json)
+  document.getElementById("issues").innerHTML = html;
 }
 
 function createIssue() {
+  var title = document.getElementById("title").value;
+  var body = document.getElementById("body").value;
+
+
+  fetch("https://api.github.com/repos/BeejLuig/javascript-fetch-lab/issues", {
+    method: 'post',
+    body: JSON.stringify({
+      title: title,
+      body: body
+    }),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(getIssues());
 }
 
 function showResults(json) {
+  var src = document.getElementById("repo-template").innerHTML;
+  var template = Handlebars.compile(src);
+  var html = template(json);
+  document.getElementById("results").innerHTML = html;
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch("https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks", {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showResults(json));
 }
 
 function getToken() {
