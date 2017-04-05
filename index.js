8b218fbe6993f6c48aaaa90f42fffe0bbeda79c3
Idayat Oshobajo <idayat.oshobajo@gmail.com>
2017-04-05 01:45:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,53 @@
 function getIssues() {
+  const repo = 'https://api.github.com/repos/Idayat8703/javascript-fetch-lab/issues'
+
+  fetch(repo, {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json())
+  .then(json => showIssues(json))
 }
 
 function showIssues(json) {
+  var template = Handlebars.compile(document.getElementById("issues-template").innerHTML)
+  document.getElementById('issues').innerHTML = template(json)
 }
 
 function createIssue() {
+  const repo = 'https://api.github.com/repos/Idayat8703/javascript-fetch-lab/issues'
+  const issue = {
+    title: document.getElementById('title').value,
+    body: document.getElementById('body').value
+  }
+
+  fetch(repo, {
+    method: 'post',
+    body: JSON.stringify(issue),
+    headers: {
+    Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json())
+  .then(json => getIssues())
 }
 
 function showResults(json) {
+  var template = Handlebars.compile(document.getElementById("repo-template").innerHTML)
+  document.getElementById('results').innerHTML = template(json)
 }
 
+
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  const repo = 'https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks'
+  fetch(repo, {
+    method: 'post',
+    headers: {
+    Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json())
+  .then(json => showResults(json))
 }
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
