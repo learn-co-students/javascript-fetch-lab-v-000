cdf6b0368336cc16f949c29a2483df799183235d
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-04-15 20:48:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 function getIssues() {
+  var issueURL = 'https://api.github.com/repos/mdjurdj1/javascript-fetch-lab/issues'
+  //using fetch
+  fetch(issueURL)
+  .then(res => res.json())
+  .then(json => showIssues(json))
 }
 
 function showIssues(json) {
+  var template = Handlebars.compile(document.getElementById("issues-template").innerHTML)
+  document.getElementById('issues').innerHTML = template(json)
 }
 
 function createIssue() {
+  const issue = {
+     title: document.getElementById('title').value,
+     body: document.getElementById('body').value
+   }
+  var issueURL = 'https://api.github.com/repos/mdjurdj1/javascript-fetch-lab/issues'
+  //using fetch
+  fetch(issueURL, {
+    method: "post",
+    headers: {
+       Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(issue)
+  }).then(res => res.json())
+  .then(json => getIssues(json))
 }
 
 function showResults(json) {
+  var template = Handlebars.compile(document.getElementById("repo-template").innerHTML)
+  document.getElementById('results').innerHTML = template(json)
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const forkUrl = 'https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks'
   //use fetch to fork it!
+  fetch(forkUrl, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json())
+  .then(json => showResults(json))
 }
 
 function getToken() {
