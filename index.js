e3dbef6cb31172ce162de7365d13a630eea0fbc8
Bob Habig <rfhab2@gmail.com>
2017-05-02 19:47:36 -0400

    Done.

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ function createIssue() {
 }
 
 function showResults(json) {
+  debugger;
+  var repoDiv = document.getElementById('repo-template').innerHTML;
+  var template = Handlebars.compile(repoDiv);
+  document.getElementById('results').innerHTML += template(repoDiv);
 }
 
 function forkRepo() {
+  const token = getToken();
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => showResults(json));
 }
 
 function getToken() {
