3f47e87089af381856d12ffb2d49050ff7a70005
Bob Habig <rfhab2@gmail.com>
2017-05-02 19:53:56 -0400

    corrected argument passed into template in showRepository()

--- index.js
+++ index.js
@@ -8,17 +8,16 @@ function createIssue() {
 }
 
 function showResults(json) {
-  debugger;
   var repoDiv = document.getElementById('repo-template').innerHTML;
   var template = Handlebars.compile(repoDiv);
-  document.getElementById('results').innerHTML += template(repoDiv);
+  document.getElementById('results').innerHTML += template(json);
 }
 
 function forkRepo() {
   const token = getToken();
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   fetch(`https://api.github.com/repos/${repo}/forks`, {
-    method: 'POST',
+    method: 'post',
     headers: {
       Authorization: `token ${token}`
     }
@@ -28,5 +27,5 @@ function forkRepo() {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '3653deff3d08d342017abe94948d9fd39d11a4f0'
 }
