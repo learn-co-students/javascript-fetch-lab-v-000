22faafc2fc8292d0a231aa42b9b6926ac49d2695
Bob Habig <rfhab2@gmail.com>
2017-05-03 00:29:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 const token = getToken();
-const newRepo = '';
+const newRepo = 'bhabig/javascript-fetch-lab';
 
 function getIssues() {
+  debugger;
 }
 
 function showIssues(json) {
+  const issuesDiv = document.getElementById('issues-template').innerHTML;
+  const template = Handlebars.compile(issuesDiv);
+  document.getElementById('issues').innerHTML += template(json);
 }
 
 function createIssue() {
   const issueTitle = document.getElementById('title').value;
   const issueBody = document.getElementById('body').value;
+  const postData = {title: issueTitle, body: issueBody};
   fetch(`https://api.github.com/repos/${newRepo}/issues`, {
     method: 'post',
+    body: JSON.stringify(postData),
     headers: {
       Authorization: `token ${token}`
     }
-  })
+  }).then(res => res.json()).then(json => showIssues(json))
 }
 
 function showResults(json) {
-  newRepo = json.full_name;
   const repoDiv = document.getElementById('repo-template').innerHTML;
   const template = Handlebars.compile(repoDiv);
   document.getElementById('results').innerHTML = template(json);
@@ -38,5 +43,5 @@ function forkRepo() {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return '351b5ea85aa8f563282605121783a45372c20dda'
 }
