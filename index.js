841e5b7f26bf15546c99b6b4eb3727f24d0c578d
WilliamMena <williammena1991@gmail.com>
2017-06-01 23:26:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const token = '8d9ff7d58b37676a4b327f8b6ec0b2b3c4f01bc1'
+
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json().then(json => console.log(json)))
+
 function getIssues() {
 }
 
