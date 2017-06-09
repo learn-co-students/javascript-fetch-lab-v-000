b423b41948a5aa00657756f6aa374f83d4e123f7
DanL12186 <danlrcn@hotmail.com>
2017-06-09 14:18:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,56 @@
-function getIssues() {
+const urlPrefix = "https://api.github.com/repos"
+const repo = `javascript-fetch-lab`
+const user = "DanL12186"
+
+function getIssues() { //no need for 'get' method or authorization; get is default and this is pubic.
+  fetch(`${urlPrefix}/${user}/${repo}/issues`)
+  .then(response => response.json()) //parse response into json format
+  .then(json => showResults(json)) //pass parsed rseponse to showResults()
 }
 
 function showIssues(json) {
+  const template = Handlebars.compile(document.getElementById("issues-template").innerHTML);
+  const issues = template(json);
+
+  document.getElementById("issues").innerHTML = issues;
 }
 
 function createIssue() {
+  const title = document.getElementById('title').value; //grabs title and body values from HTML
+  const body = document.getElementById('body').value;
+  const postData = { //places it into a postData variable
+    body: body,
+    title: title
+  }
+
+  fetch(`${urlPrefix}/${user}/${repo}/issues`, { //fetches URL for this repo
+    method: 'post',
+    body: JSON.stringify(postData),   //sets body to above postData; JSON.stringify necessary for it to work
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(resp => getIssues())
 }
 
-function showResults(json) {
+function showResults(json) { //called by forkRepo's last line, receives json as argument
+  const template = Handlebars.compile(document.getElementById("repo-template").innerHTML);
+  const results = template(json);  //creates template with json argument data
+  //console.log(json)
+  document.getElementById("results").innerHTML = results;  //fills results div with json data
 }
 
-function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+function forkRepo() { //forks repository
+  fetch(`${urlPrefix}/learn-co-curriculum/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}` //"Authorization" a required word
+    }
+  })
+  .then(response => response.json()) //parse response into json format
+  .then(json => showResults(json)) //pass parsed rseponse to showResults()
+
 }
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
   return ''
 }
