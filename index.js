fbc1bc6ca5c7e072be3c6784a7a66a60c484cd83
John Pfingst <jpfingst@outlook.com>
2017-06-11 19:10:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const owner  = 'jcpny1';
+const repo   = 'javascript-fetch-lab';
+const source = 'learn-co-curriculum';
+
 function getIssues() {
 }
 
@@ -5,26 +9,41 @@ function showIssues(json) {
 }
 
 function createIssue() {
+  fetch(`https://api.github.com/repos/${owner}/${repo}/issues`, {
+    method: 'POST',
+    headers: { Authorization: `token ${getToken()}` },
+    body: JSON.stringify({title: $('#title').val(), body: $('#body').val()})
+  }).then(function(response) {
+    if(response.ok) { return Promise.resolve(response) }
+    return response.json().then(json => {
+      const error = new Error(json.message || response.statusText);
+      displayError(error.message);
+    })
+  })
 }
 
 function showResults(json) {
   console.log(json);
+  const template = Handlebars.compile($('#repo-template').html());
+  $('#results').html(template(json));
 }
 
 function forkRepo() {
-  const owner = 'learn-co-curriculum';
-  const repo  = 'javascript-fetch-lab';
   //use fetch to fork it!
-  fetch(`https://api.github.com/repos/${owner}/${repo}/forks`, {
+  fetch(`https://api.github.com/repos/${source}/${repo}/forks`, {
     method: 'POST',
     headers: { Authorization: `token ${getToken()}` }
-  }).then(res => res.json()).then(json => showResults(json));
-  // const token = getToken();
-  // fetch('https://api.github.com/user/repos', {
-  //   headers: {
-  //     Authorization: `token ${token}`
-  //   }
-  // }).then(res => res.json()).then(json => console.log(json));
+  }).then(function(response) {
+    if(response.ok) { return response }
+    throw new Error('Network response was not ok.')
+  }).then(res => res.json()).then(json => showResults(json)
+   ).catch(function(error) {
+     displayError(error.message)
+   });
+}
+
+function displayError(msg) {
+  $('#errors').html('There has been a problem with your fetch operation: ' + msg)
 }
 
 function getToken() {
