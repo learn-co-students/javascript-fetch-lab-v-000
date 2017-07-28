5b48a89aae3bdf02931e7831f244821967814da8
Howard DeVennish <howarddevennish@gmail.com>
2017-07-28 17:44:04 -0400

    about halfway done

--- index.js
+++ index.js
@@ -1,19 +1,70 @@
+const fork = 'howardbdev/javascript-fetch-lab'
+
 function getIssues() {
+  fetch(`https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/issues`, {
+    method: 'GET',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(results => console.log(results));
 }
 
 function showIssues(json) {
+  let template = Handlebars.compile(document.getElementById('issues-template').innerHTML);
+  let issue = template(json);
+  document.getElementById('issues').append("<br>" + issue + "<br>");
 }
 
 function createIssue() {
+  var postData = {title: document.getElementById('title').val(), body: document.getElementById('body').val()}
+  debugger
+  fetch(`https://api.github.com/repos/howardbdev/javascript-fetch-lab/issues`, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(results => results.json())
+    .then(results => console.log(results));
 }
 
 function showResults(json) {
 }
 
+function Repo(attributes){
+  this.attributes = attributes;
+  this.url = attributes.url;
+}
+
+Repo.prototype.template = function() {
+  var template = `<h3>Forked Successfully!</h3><a href="${this.url}"> ${this.url}</a>`
+  return template;
+};
+
+const baseApi = 'https://api.github.com/'
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+
+  fetch(`${baseApi}repos/${repo}/forks`, {
+    // POST /repos/:owner/:repo/forks
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`,
+    }
+  }).then(resp => resp.json())
+
+    .then(resp => {
+    let repo = new Repo(resp);
+    showForkedRepo(repo);
+  })
+
 }
+
+function showForkedRepo(repo) {
+  // console.log("in the showForkedRepo function, repo: " + repo)
+  document.getElementById('results').append(repo.template())
+}
+
 
 function getToken() {
   //change to your token to run in browser, but set
