d18240fa14ae4de1ae81495f109ce4f302c622c0
ETphonehomeslice <mk.etlinger@gmail.com>
2017-08-11 14:04:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
 function getIssues() {
+  fetch('https://api.github.com/repos/mk-etlinger/javascript-fetch-lab/issues')
+  .then(response => response.json()).then(json => showIssues(json))
 }
 
 function showIssues(json) {
+  const el = document.getElementById("issues-template").innerHTML
+  const template = Handlebars.compile(el)
+  const repoList = template(json)
+  debugger;
+  document.getElementById("issues").innerHTML = repoList
 }
 
 function createIssue() {
+  const title = document.getElementById("title").value
+  const body = document.getElementById("body").value
+  const postData = { title: title, body: body }
+  fetch('https://api.github.com/repos/mk-etlinger/javascript-fetch-lab/issues', {
+    method: 'POST',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(getIssues())
 }
 
 function showResults(json) {
+  const el = document.getElementById("repo-template").innerHTML
+  const template = Handlebars.compile(el)
+  const repoList = template(json)
+  document.getElementById("results").innerHTML = repoList
 }
 
 function forkRepo() {
-  const repo = 'https://api.github.com/repos/Mk-Etlinger/javascript-fetch-lab/forks'
+  const repo = 'https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks'
   fetch(repo, {
     method: 'POST',
     headers: {
-      'Authorization': `token: ${getToken()}`
+      'Authorization': `token ${getToken()}`
     }
-  }).then(response => response.json()).then(json => console.log(json))
+  }).then(response => response.json()).then(json => showResults(json))
+}
+
+function showForkedRepo(json) {
+
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return '50e94b3d681be4dc1bcd33e20c54ab0242d03f4d'
+  return ''
 }
