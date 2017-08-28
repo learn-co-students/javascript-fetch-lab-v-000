30560933ed030c0187d40201a4a8a2afbc149b24
Luisa Scavo <luisascavo@gmail.com>
2017-08-28 20:17:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-function getIssues() {
-}
+const username = 'weezwo'
+const rootURI = 'https://api.github.com/'
+const fork = `${username}/javascript-fetch-lab`
 
-function showIssues(json) {
+function getIssues() {
+  fetch(`${rootURI}repos/${fork}/issues`).
+    then(resp => {
+      resp.json().then( data => {
+        for (let i = 0; i < data.length; i++){
+          displayIssue(new Issue(data[i]));
+        }
+      } )
+    })
 }
 
 function createIssue() {
-}
-
-function showResults(json) {
+  const title = document.getElementById('title').value;
+  const body = document.getElementById('body').value;
+  fetch(`${rootURI}${fork}/issues`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify({title: title, body: body})
+  }).then(res => getIssues());
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(`${rootURI}repos/${repo}/forks`, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
 }
 
 function getToken() {
