a01aab6537c8199d563295f7e6c735015ee0abcd
Bruce Molina <bruce.molina.81@gmail.com>
2017-09-07 02:30:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,79 +1,91 @@
-// //the token to use  70510ae4478d63c234bb777aa59304fb9005e1a7
-//
-// // const token = '70510ae4478d63c234bb777aa59304fb9005e1a7';
-//
-// //GET EXAMPLE
-// // fetch('https://api.github.com/user/repos', {
-// //   headers: {
-// //     Authorization: `token ${token}`
-// //   }
-// // }).then(res => res.json()).then(json => console.log(json));
-//
-//
-// // const token = '70510ae4478d63c234bb777aa59304fb9005e1a7';
-// // const postData = {
-// //   body: 'Great stuff'
-// // };
-// //
-// // // POST example with fetch()
-// // fetch('https://api.github.com/repos/bmolina-nyc/javascript-git-hub-api-with-xhr-lab-v-000/commits/80fc02318a800ef28f96f2d69c4e7f7d787d6b5e/comments', {
-// //   method: 'POST',
-// //   body: JSON.stringify(postData),
-// //   headers: {
-// //     Authorization: `token ${token}`
-// //   }
-// // }).then(res => console.log(res));
-// //
-// //
-//
-function getIssues() {
+const userName = 'bmolina-nyc'
+const baseApi = 'https://api.github.com/'
+const fork = `${userName}/javascript-fetch-lab`
+
+//Issue and Repo objects and templates
+
+class Issue{
+  constructor(attributes){
+    this.title = attributes.title;
+    this.body = attributes.body;
+    this.url = attributes.url;
+  }
+
+  template(){
+     var template = `<li>Title: <a href="${this.url}">${this.title} </a><span> | Body: ${this.body}</span></li>`
+     return template;
+  };
 }
 
-function showIssues(json) {
+class Repo{
+  constructor(attributes){
+    this.url = attributes.url;
+  }
+
+  template(){
+    var template = `<h3>Forked Successfully!</h3><a href="${this.url}"> ${this.url}</a>`
+    return template;
+  };
 }
+
+//Create an issue through the Github API
 
 function createIssue() {
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+  const postData = { title: issueTitle, body: issueBody }
+  fetch(`${baseApi}repos/${fork}/issues`, {
+    method: 'post',
+    headers: {
+      'Authorization': `token ${getToken()}`
+    },
+    body: JSON.stringify(postData)
+  }).then(resp => getIssues())
 }
 
-// API call to GET the elements
+//Fetch all issues through the Github API and display / append to the DOM
+
+function getIssues(data) {
+  fetch(`${baseApi}repos/${fork}/issues`).
+    then(resp => {
+      resp.json().then( data => {
+        console.log(data[0])
+        for (let i = 0; i < data.length; i++){
+          displayIssue(new Issue(data[i]));
+        }
+      } )
+    })
+}
+
+function displayIssue(issue) {
+  $('#issues').append(issue.template())
+}
+
+//Fetch and show repo info
+
+
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
 
-  fetch(`https://api.github.com/repos/${repo}/forks`, {
+  fetch(`${baseApi}repos/${repo}/forks`, {
+    method: 'post',
     headers: {
-      Authorization: `token ${getToken()}`
+      'Authorization': `token ${getToken()}`
     }
-  }).
-  then(res => res.json()).
-  then(json => showResults(json));
+  }).then(resp => {
+    let repo = new Repo(resp);
+    showForkedRepo(repo);
+  })
 }
 
-
-// break down the items into single elements
-function showResults(json) {
-  let result = json.map( el => showResult(el) ).join('')
-  return `<ul> ${result} </ul>`
-}
-
-// render the single elements to the map
-function showResult(el){
-    `
-      ${$('#results').append(`
-        <li>
-          ${el.owner.login}  - Open Issues: ${el.open_issues_count}
-
-        </li>
-        <br>
-      `)}
-    `
-    console.log(el)
+function showForkedRepo(repo) {
+  $('#results').append(repo.template())
 }
 
 // github authorization
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  // // return ''
-  // return '70510ae4478d63c234bb777aa59304fb9005e1a7'
+  // return ''
+  return '7a78f6baa2d81da329bd382022c113c38a4c9f96'
 }
