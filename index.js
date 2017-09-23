c02c7d8629bd38e3060a7a3424ceb6f2fa48cf73
dustin <dandersonsoftware@outlook.com>
2017-09-23 14:07:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 function getIssues() {
+  fetch('/api.github.com/repos/javascript-fetch-lab/issues', {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
 }
 
 function showIssues(json) {
 }
 
 function createIssue() {
+  var issueTitle = document.getElementById("title").value;
+  var issueBody = document.getElementById("body").value;
+  const issueData = {
+    title: issueTitle,
+    body: issueBody
+  }
+  fetch('/api.github.com/repos/javascript-fetch-lab/issues', {
+    method: 'post',
+    body: JSON.stringify(issueData),
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => console.log(res));
 }
 
 function showResults(json) {
@@ -13,6 +31,12 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch('/api.github.com/repos/' + repo, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  })
 }
 
 function getToken() {
