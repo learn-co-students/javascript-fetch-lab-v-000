f2b57a54514305abe53064abfd2ee0339a27d24e
Tina Hang <mstinahang@gmail.com>
2018-02-13 03:18:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
 function getIssues() {
+  fetch('javascript-fetch-lab/issues', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(resp => resp.json())
+    .then(json => showIssues(json))
 }
 
 function showIssues(json) {
+  const template = Handlebars.complile(document.getElementById('issues-template').innerHTML);
+  document.getElementById('issues').innerHTML = template(json);
 }
 
 function createIssue() {
+  fetch('javascript-fetch-lab/issues', {
+    method: 'post',
+    body: 'test body',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(resp => getIssues())
 }
 
 function showResults(json) {
+  const template = Handlebars.compile(document.getElementById('repo-template').innerHTML);
+  document.getElementById('results').innerHTML = template(json);
 }
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch('/api.github.com/repos/' + repo, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(resp => resp.json())
+    .then(json => showResults(json))
 }
 
 function getToken() {
@@ -20,3 +44,5 @@ function getToken() {
   //back to '' before committing so all tests pass
   return ''
 }
+
+const token = getToken();
